df63a017a3996b716be2d0338efc705294630c0e
Shrihari-Nizamkar <shriharinzm@gmail.com>
2025-07-21 10:32:25 +0530

    task completed

---
+++ Controllers/url.js
@@ -0,0 +1,36 @@
+import shortid from "shortid";
+import { Url } from "../Models/Url.js";
+
+export const shortUrl = async(req, res) =>{
+    const longUrl = req.body.longUrl
+    const shortCode = shortid.generate()
+
+    // console.log('longurl =', longUrl)
+    // console.log('shortcode =', shortCode)
+
+    const shortUrl = `http://localhost:3000/${shortCode}`
+
+    // save to database
+    const newUrl = new Url({shortCode, longUrl})
+    await newUrl.save()
+
+    console.log('short url saved = ', newUrl)
+
+    res.render("index.ejs", {shortUrl})
+    
+
+}
+
+export const getOriginalUrl = async(req,res) =>{
+    const shortCode = req.params.shortCode
+
+    // find code into database
+    const originalUrl = await Url.findOne({shortCode})
+
+    if(originalUrl){
+        res.redirect(originalUrl.longUrl)
+    }
+    else{
+        res.json({msg:'invalid Shortcode'})
+    }
+}
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import { getOriginalUrl, shortUrl } from './Controllers/url.js'
+
+
 
 const app = express()
+
+app.use(express.urlencoded({extended:true}))
+
 
 const url = "mongodb://localhost:27017/"
 
@@ -10,6 +16,19 @@ mongoose.connect(url,
     .then(()=>console.log('MongoDB Connected....'))
     .catch((err)=>console.log('Error :', err))
 
+
+
+// rendering ejs file
+app.get("/", (req, res)=>{
+    res.render('index.ejs', {shortUrl:null})
+})
+
+
+//shortening url - to save url into database
+app.post('/short', shortUrl)
+
+// redirect to original url
+app.get('/:shortCode', getOriginalUrl)
 
 const port = 3000;
 
